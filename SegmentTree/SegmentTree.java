f20357e4bba480bdb25a7c60ea443a9ece9ca5b9
chind <dinesh.chintala@pega.com>
2020-05-15 09:20:39 +0530

    initial skeleton code

---
+++ SegmentTree/SegmentTree.java
@@ -0,0 +1,45 @@
+import java.util.function.Function;
+
+
+public class SegmentTree {
+
+private Function<T,T> func;
+
+public void SegmentTree() {
+ 
+ init();
+}
+
+public void SegmentTree(int[] array, Function<T,T> func){
+
+
+init();
+}
+
+
+private void init(){
+
+
+}
+
+
+public SegmentTree build() {
+
+
+
+}
+
+public SegmentTree update(){
+
+
+}
+
+
+public T findValue() {
+
+
+
+}
+
+
+}
